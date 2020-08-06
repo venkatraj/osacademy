9d9aa3d3f8b8a1217a8f1f8edcc0059ee7bb5188
unknown <venkat@codinggeek.com>
2020-08-06 12:44:57 +0530

    adding permission layer

--- api/permissions/index.ts
+++ api/permissions/index.ts
@@ -1,11 +1,53 @@
-const protectedPaths = [
-  'Query.users',
-  'Query.me',
-  'Mutation.createCourse',
-  'Mutation.editCourse',
-  'Mutation.deleteCourse',
-  'Mutation.deleteUser',
-  'Mutation.editUser',
-]
+import { rule, shield, or, deny, not, allow } from 'nexus-plugin-shield'
+import { getUserId } from '../utils'
 
-export default protectedPaths
+const isAuthenticated = rule({ cache: 'contextual' })(
+  async (_root, _args, ctx) => {
+    const userId = getUserId(ctx.token)
+    return Boolean(userId)
+  },
+)
+
+const isAdmin = rule({ cache: 'contextual' })(async (_root, _args, ctx) => {
+  const userId = getUserId(ctx.token)
+  const user = await ctx.db.user.findOne({
+    where: {
+      id: userId,
+    },
+  })
+  return user.role === 'Admin'
+})
+
+const isInstructor = rule({ cache: 'contextual' })(
+  async (_root, _args, ctx) => {
+    const userId = getUserId(ctx.token)
+    const user = await ctx.db.user.findOne({
+      where: {
+        id: userId,
+      },
+    })
+    return user.role === 'Instructor'
+  },
+)
+
+const permissions = shield({
+  rules: {
+    Query: {
+      me: isAuthenticated,
+      users: isAdmin,
+    },
+    Mutation: {
+      '*': isAuthenticated,
+      login: allow,
+      signup: allow,
+      createCourse: or(isAdmin, isInstructor),
+      editCourse: or(isAdmin, isInstructor),
+      deleteCourse: or(isAdmin, isInstructor),
+      createUser: isAdmin,
+      editUser: or(isAuthenticated, isAdmin),
+      deleteUser: isAdmin,
+    },
+  },
+})
+
+export { permissions }
